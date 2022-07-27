be0b3395df0a85b2136bc4ad64ddf97879a55d60
Ashiq Legi <ashiqlegi@gmail.com>
2022-07-27 17:56:04 -0500

    Done

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  fetch("https://anapioficeandfire.com/api/books")
+    .then(response => response.json())
+    .then(data => {
+      renderBooks(data)
+    })
+
 }
+  
+    
+ 
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -15,3 +22,6 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+
+//need to return fetch
